e1b1f47fc25afb10ee3f18cbe27f3391caeb75b8
Hugues Evard <hugues.evard@cern.ch>
2022-09-30 13:35:57 +0200

    Modified files to train / test on and number of epochs in train_VBF script

--- train_VBF.sh
+++ train_VBF.sh
@@ -20,12 +20,12 @@ lr="1e-3"
 extraopts=""
 
 weaver \
-    --data-train "${DATADIR}/MC__*_M130_*_UL18_[1-9].root" \
-    --data-test "${DATADIR}/MC__*_M130_*_UL18_[1-9].root" \
+    --data-train "${DATADIR}/MC__*_M130_*_UL18_[1-2][0-9].root" \
+    --data-test "${DATADIR}/MC__*_M130_*_UL18_3[0-9].root" \
     --data-config config/VBFparticlenet.yaml --network-config $modelopts \
     --model-prefix training/VBF/{auto}${suffix}/net \
     --num-workers 1 --fetch-step 1 --in-memory --train-val-split 0.8889 \
-    --batch-size 512 --samples-per-epoch 160000 --samples-per-epoch-val 20000 --num-epochs 2 \
+    --batch-size 512 --samples-per-epoch 160000 --samples-per-epoch-val 20000 --num-epochs 20 \
     --start-lr $lr --optimizer ranger --log logs/VBF_{auto}${suffix}.log --predict-output pred.root \
     --tensorboard VBF_${suffix} \
     ${extraopts} "${@:3}"
