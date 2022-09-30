99752d5fe476508f89e22ee82e00d19048131cbc
anmalara <andrea.malara@cern.ch>
2022-09-30 20:10:06 +0200

    generalised bash script

--- train_VBF.sh
+++ train_VBF.sh
@@ -1,40 +1,55 @@
 #!/bin/bash
 
-MODEL=$1
-DATA_CARD=$2
+# Example of usage
+#./train_VBF.sh
+#./train_VBF.sh -m deepak8_pf
+#./train_VBF.sh -d VBF_points_features -m particlenet_pf
+#./train_VBF.sh -e "--batch-size 512 --num-epochs 3"
 
 set -x
 
-#export DATADIR_VBF='/eos/user/h/hevard/datasets/test'
-export DATADIR_VBF='/eos/user/a/anmalara/Public/DNNInputs/'
-
 echo "args: $@"
 
-# set the dataset dir via `DATADIR_QuarkGluon`
-DATADIR=${DATADIR_VBF}
-[[ -z $DATADIR ]] && DATADIR='./datasets/QuarkGluon'
+extra_opts=""
 
-# set a comment via `COMMENT`
-suffix=${COMMENT}
+model="mlp_pf"
+data="VBF_features"
 
-# PN, PFN, PCNN, ParT
-#modelopts="networks/particlenet_pf.py"
-lr="1e-3"
-extraopts=""
+while getopts m:d:e: flag
+do
+    case "${flag}" in
+        m) model=${OPTARG};;
+        d) data=${OPTARG};;
+        e) extra_opts=${OPTARG};;
+    esac
+done
+
+
+inputdir="/eos/home-a/anmalara/Public/DNNInputs"
+outputdir="trainings/${model}/{auto}"
+output_name="pred.root"
+
+data_train="${inputdir}/MC__*_M130_*_UL18_[8-9].root"
+data_val="${inputdir}/MC__*_M130_*_UL17_[8-9].root"
+data_test="${inputdir}/MC__*_M130_*_UL16postVFP_[8-9].root"
+
+model_config="models/${model}.py"
+data_config="data/${data}.yaml"
+
+train_opts="--num-epochs 2"
+batch_opts="--batch-size 128 --start-lr 1e-3"
 
 weaver \
-    --data-train "${DATADIR}/MC__*_M130_*_UL18_1.root" \
-    --data-test "${DATADIR}/MC__*_M130_*_UL18_2.root" \
-    --data-config $DATA_CARD --network-config $MODEL \
-    --model-prefix training/VBF/{auto}${suffix}/net \
-    --num-workers 1 --fetch-step 1 --in-memory --train-val-split 0.8889 \
-    --batch-size 512 --samples-per-epoch 160000 --samples-per-epoch-val 20000 --num-epochs 20 \
-    --start-lr $lr --optimizer ranger --log logs/VBF_{auto}${suffix}.log --predict-output pred.root \
-    --tensorboard VBF_${suffix} \
-    ${extraopts} "${@:3}"
-#    --batch-size 512 --samples-per-epoch 16000 --samples-per-epoch-val 2000 --num-epochs 2 --gpus 0 \
-#    --data-train "${DATADIR}/MC__*_M130_*_UL18_1.root" \
-#    --data-test "${DATADIR}/MC__*_M130_*_UL18_2.root" \
-#    --data-train "${DATADIR}/MC__*_M130_*_UL18_[1-2][0-9].root" \
-#    --data-test "${DATADIR}/MC__*_M130_*_UL18_3[0-9].root" \
-#    --data-config data/VBF_points_features.yaml --network-config $modelopts \
+    --data-train ${data_train} --data-val ${data_val} --data-test ${data_test} \
+    --data-config ${data_config} --network-config ${model_config} \
+    --model-prefix "${outputdir}/net" --log "${outputdir}/log.log" \
+    --num-workers 2 --fetch-step 1 ${train_opts} ${batch_opts} --gpus "" \
+    --optimizer ranger --predict-output ${output_name} \
+    ${extra_opts}
+
+echo cp ${PWD}/$0 ${outputdir}/$0
+
+#--samples-per-epoch 160000
+#--samples-per-epoch-val 20000
+#--tensorboard VBF_${suffix} \
+#--in-memory --train-val-split 0.8889
