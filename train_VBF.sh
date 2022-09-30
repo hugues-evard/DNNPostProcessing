5214721a67037118c731728849a6bab8942c5329
Hugues Evard <hugues.evard@cern.ch>
2022-09-30 12:22:04 +0200

    Modified the dataset directory in train_VBF script Added new variables, available in the new root files, in the VBFparticlenet.yaml data card Added condor job outputs to gitignore

--- train_VBF.sh
+++ train_VBF.sh
@@ -2,7 +2,8 @@
 
 set -x
 
-export DATADIR_VBF='/eos/user/h/hevard/datasets/test'
+#export DATADIR_VBF='/eos/user/h/hevard/datasets/test'
+export DATADIR_VBF='/eos/user/a/anmalara/Public/DNNInputs/'
 
 echo "args: $@"
 
@@ -19,13 +20,15 @@ lr="1e-3"
 extraopts=""
 
 weaver \
-    --data-train "${DATADIR}/MC__*_M125_standard_UL18_1_test.root" \
-    --data-test "${DATADIR}/MC__*_M125_standard_UL18_1_test.root" \
+    --data-train "${DATADIR}/MC__*_M130_*_UL18_[1-9].root" \
+    --data-test "${DATADIR}/MC__*_M130_*_UL18_[1-9].root" \
     --data-config config/VBFparticlenet.yaml --network-config $modelopts \
     --model-prefix training/VBF/{auto}${suffix}/net \
     --num-workers 1 --fetch-step 1 --in-memory --train-val-split 0.8889 \
-    --batch-size 512 --samples-per-epoch 16000 --samples-per-epoch-val 2000 --num-epochs 2 \
+    --batch-size 512 --samples-per-epoch 160000 --samples-per-epoch-val 20000 --num-epochs 2 \
     --start-lr $lr --optimizer ranger --log logs/VBF_{auto}${suffix}.log --predict-output pred.root \
     --tensorboard VBF_${suffix} \
     ${extraopts} "${@:3}"
 #    --batch-size 512 --samples-per-epoch 16000 --samples-per-epoch-val 2000 --num-epochs 2 --gpus 0 \
+#    --data-train "${DATADIR}/MC__*_M125_standard_UL18_1_test.root" \
+#    --data-test "${DATADIR}/MC__*_M125_standard_UL18_1_test.root" \
